e4ae3a490f3944eabf22203d92d5345fb4ceed15
Bhumika Kumari <128302166+Bhumikakri@users.noreply.github.com>
2023-12-30 19:13:35 +0530

    pending task

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import PageLoad from './component/LoadingPage';
 import NotePage from './component/notePage';
 
 
 function App() {
-  const[userInfo,setuserInfo] = useState({id:'',body:''});
+  const[userInfo,setuserInfo] = useState([{id:"",body:""}]);
+  
+console.log(userInfo,"in app.js");
+  // useEffect(()=>{
+   
+  //   if(localStorage.getItem("notes")===null){
+  //      localStorage.setItem("notes",JSON.stringify([...userInfo]));
+  //   }
+  // },[userInfo])
 
-  useEffect(()=>{
-    localStorage.setItem("notes",JSON.stringify({...userInfo}));
-  },[userInfo.id])
   
   return (
     <div className="App">
