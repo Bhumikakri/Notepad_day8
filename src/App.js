30b6d4416962bc6bc06395aee6c703f462ea447b
Bhumika Kumari <128302166+Bhumikakri@users.noreply.github.com>
2023-12-31 15:43:29 +0530

    navbar functionality left

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import NotePage from './component/notePage';
 
 
 function App() {
+  // State to manage user information
   const[userInfo,setuserInfo] = useState([{id:"",body:""}]);
   
-console.log(userInfo,"in app.js");
-  // useEffect(()=>{
-   
+  // console.log(userInfo,"in app.js")
+  // useEffect(()=>{ 
   //   if(localStorage.getItem("notes")===null){
   //      localStorage.setItem("notes",JSON.stringify([...userInfo]));
   //   }
   // },[userInfo])
 
   
+  // Conditional rendering based on localStorage
   return (
     <div className="App">
      {localStorage.getItem("notes")==null ? <PageLoad info={{userInfo,setuserInfo}}/>:<NotePage info={{userInfo,setuserInfo}}/>}
