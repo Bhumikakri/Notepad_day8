b4d753f7bb756171dd93afcfa73cc33dc98d5f14
Bhumika Kumari <128302166+Bhumikakri@users.noreply.github.com>
2024-01-02 13:48:33 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import { useState } from "react";
-import "./App.css";
-import PageLoad from "./component/LoadingPage";
-import NotePage from "./component/notePage";
+import { useState } from 'react';
+import './App.css';
+import PageLoad from './component/LoadingPage';
+import NotePage from './component/notePage';
+
 
 function App() {
   // State to manage user information
-  const [userInfo, setuserInfo] = useState([{ id: "", body: "" }]);
-  const [value, setValue] = useState(true);
+  const[userInfo,setuserInfo] = useState([{id:"",body:""}]);
+  
+  // console.log(userInfo,"in app.js")
+  // useEffect(()=>{ 
+  //   if(localStorage.getItem("notes")===null){
+  //      localStorage.setItem("notes",JSON.stringify([...userInfo]));
+  //   }
+  // },[userInfo])
 
   
+  // Conditional rendering based on localStorage
   return (
     <div className="App">
-      {value === true ? (
-        <PageLoad set={{ value, setValue }} info={{ userInfo, setuserInfo }} />
-      ) : (
-        <NotePage set={{ value, setValue }} info={{ userInfo, setuserInfo }} />
-      )}
+     {localStorage.getItem("notes")==null ? <PageLoad info={{userInfo,setuserInfo}}/>:<NotePage info={{userInfo,setuserInfo}}/>}
     </div>
   );
 }
